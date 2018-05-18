e1fcf62574579610064ffd06d7a99fcb703fdeb0
Jon Maldia <jon@maldia.net>
2018-05-18 12:19:16 -0400

    Adds function to dynamically add images on back of each card

--- js/app.js
+++ js/app.js
@@ -1,8 +1,40 @@
+const box = $('.box');
+const board = $('.container');
+let imagePaths = [
+    'img/01-car-sml.png',
+    'img/02-car-sml.png',
+    'img/03-car-sml.png',
+    'img/04-car-sml.png',
+    'img/05-car-sml.png',
+    'img/06-car-sml.png',
+    'img/07-car-sml.png',
+    'img/08-car-sml.png'
+];
+
+$(document).ready(function() {
+    for (let image = 0; image < imagePaths.length; image++) {
+        let boxHTML = `
+            <div class="box">
+            <div class="inner-box">
+                <div class="card front">
+                    <i class="far fa-gem"></i>
+                </div>
+                <div class="card back">
+                    <img src="${imagePaths[image]}">
+                </div>
+            </div>
+            </div>
+        `
+        board.append(boxHTML);
+        board.append(boxHTML);
+
+        console.log(boxHTML);
+    }
+});
+
 // Flip Card on click
-let box = $('.box');
-
-box.click(function(e) {
+board.on('click', '.box', function(e) {
     e.preventDefault;
-
     $(this).find('.inner-box').toggleClass('flip');
+    // $(this).find('.back').addClass('grow').delay(3000).removeClass('grow');
 }); 
